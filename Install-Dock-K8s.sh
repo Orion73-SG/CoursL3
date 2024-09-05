a2582e2a9df1c9661d9452da9a04272efd9a77bf
Seb Portable <sebastien.giguet@ynov.com>
2024-09-05 16:06:26 +0200

    feat

--- Install-Dock-K8s.sh
+++ Install-Dock-K8s.sh
@@ -2,6 +2,19 @@
 
 # Demande du nom de la machine à l'utilisateur
 read -p "Veuillez entrer le nom de la machine (ou appuyez sur Entrée pour passer) : " hostname
+if [ -z "$hostname" ]; then
+    echo "Aucun nom de machine fourni. La configuration de l'hôte est passée."
+else
+    # Changer le nom de la machine
+    sudo hostnamectl set-hostname "$hostname"
+    echo "Le nom de la machine a été changé en : $hostname"
+
+    # Mettre à jour le fichier /etc/hosts
+    if grep -q "127.0.1.1" /etc/hosts; then
+        sudo sed -i "s/^127.0.1.1.*/127.0.1.1 $hostname/g" /etc/hosts
+    else
+        echo "127.0.1.1 $hostname" | sudo tee -a /etc/hosts > /dev/null
+    fi
 
 echo "##########"
 echo "Commençons par la configuration réseau"
@@ -11,27 +24,6 @@ CURRENT_IP=$(ip -4 addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -n 1)
 CURRENT_MASK=$(ip -o -f inet addr show | awk '/scope global/ {print $4}' | cut -d/ -f2 | head -n 1)
 CURRENT_GATEWAY=$(ip route | grep default | awk '{print $3}')
 CURRENT_DNS=$(nmcli dev show | grep 'IP4.DNS' | awk '{print $2}' | head -n 1)
-
-# Fonction pour demander confirmation ou changement de chaque paramètre
-#function ask_user {
-#    local prompt="$1"
- #   local default_value="$2"
-  #  local user_input
-#
- #   read -p "$prompt (actuel: $default_value) [Appuyez sur Entrée pour conserver, ou entrez un nouveau] : " user_input
-#
- #   if [ -z "$user_input" ]; then
-  #      echo "$default_value"
-   # else
-    #    echo "$user_input"
-    #fi
-#}
-
-# Demander à l'utilisateur de vérifier ou de changer les paramètres
-#IP=$(ask_user "Voulez-vous changer l'adresse IP ?" $CURRENT_IP)
-#MASK=$(ask_user "Voulez-vous changer le masque de sous-réseau ?" $CURRENT_MASK)
-#GATEWAY=$(ask_user "Voulez-vous changer la passerelle par défaut ?" $CURRENT_GATEWAY)
-#DNS=$(ask_user "Voulez-vous changer l'adresse DNS ?" $CURRENT_DNS)
 
 # Afficher les paramètres choisis
 echo -e "\nLes paramètres réseau configurés sont :"
@@ -53,29 +45,17 @@ echo "DNS : $DNS"
 #echo "Configuration réseau mise à jour avec succès."
 
 # Mise à jour du nom de la machine et du fichier /etc/hosts
-if [ -z "$hostname" ]; then
-    echo "Aucun nom de machine fourni. La configuration de l'hôte est passée."
-else
-    # Changer le nom de la machine
-    sudo hostnamectl set-hostname "$hostname"
-    echo "Le nom de la machine a été changé en : $hostname"
 
-    # Mettre à jour le fichier /etc/hosts
-    if grep -q "127.0.1.1" /etc/hosts; then
-        sudo sed -i "s/^127.0.1.1.*/127.0.1.1 $hostname/g" /etc/hosts
-    else
-        echo "127.0.1.1 $hostname" | sudo tee -a /etc/hosts > /dev/null
-    fi
 
     # Ajouter l'entrée pour la nouvelle adresse IP
-    if grep -q "$IP" /etc/hosts; then
-        sudo sed -i "s/^$IP.*/$IP $hostname/g" /etc/hosts
-    else
-        echo "$IP $hostname" | sudo tee -a /etc/hosts > /dev/null
-    fi
+#    if grep -q "$IP" /etc/hosts; then
+#        sudo sed -i "s/^$IP.*/$IP $hostname/g" /etc/hosts
+#    else
+#        echo "$IP $hostname" | sudo tee -a /etc/hosts > /dev/null
+#   fi
     
-    echo "Le fichier /etc/hosts a été mis à jour."
-fi
+#    echo "Le fichier /etc/hosts a été mis à jour."
+#fi
 echo "###############################"
 echo "Mise à jour"
 sudo yum update -y 
